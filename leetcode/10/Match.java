4db91b95bfba9a20dede853ae0e50e7a6a1b1609
AutoXC <43327341+AutoXC@users.noreply.github.com>
2019-08-07 22:05:00 +0800

    Create Match.java

---
+++ leetcode/10/Match.java
@@ -0,0 +1,53 @@
+package no10;
+
+public class Match {
+    public static void main(String[] args) {
+        
+    }
+    /**
+     * s 可能为空，且只包含从 a-z 的小写字母。
+     * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *，其中 '.' 匹配任意单个字符
+     *                                                    '*' 匹配零个或多个前面的那一个元素
+     */
+    public static boolean isMatch(String s, String p) {
+        if(p.length()==0&&s.length()!=0) return false;
+        if(p.length()==0&&s.length()==0) return true;
+
+        if(p.length()!=0&&s.length()==0) {
+            int ip = p.length() - 1;
+            if(p.charAt(ip)=='.'||('a'<=p.charAt(ip)&&p.charAt(ip)<='z')) return false;
+            else return true;
+        }
+        
+        int is = s.length() - 1;
+        int ip = p.length() - 1;
+        boolean ans = false;
+        while(is>=0&&ip>=0) {
+            if('a'<=p.charAt(ip)&&p.charAt(ip)<='z') {
+                if(s.charAt(is)==p.charAt(ip)) {ans = true;}
+                else {return false;}
+            }
+            else if(p.charAt(ip)=='.') {ans = true;}
+            else {
+                if(p.charAt(--ip)=='.') {return true;}
+                else {
+                    if(s.charAt(is)!=p.charAt(ip)) {
+                        ans = true;
+                        ip--;
+                        continue;
+                    }else {
+                        while(0<=is&&s.charAt(is)==p.charAt(ip)) {
+                            is--;
+                        }
+                        ans = true;
+                        ip--;
+                        continue;
+                    }
+                }
+            }
+            is--;
+            ip--;
+        }
+        //考虑is和ip值
+    }
+}

f52af8eeede584fe6b404fc9912f22763d0382b1
AutoXC <43327341+AutoXC@users.noreply.github.com>
2019-08-11 20:34:17 +0800

    Create ListNode.java

---
+++ leetcode/23/ListNode.java
@@ -0,0 +1,14 @@
+package no23;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    
+    public ListNode(int x) {
+        val = x;
+    }
+
+    public ListNode() {
+        
+    }
+}

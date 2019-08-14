c542663d11db1889a32d988e462b75228db9226c
AutoXC <43327341+AutoXC@users.noreply.github.com>
2019-08-14 10:04:56 +0800

    Update NoSameNum.java

--- leetcode/46/NoSameNum.java
+++ leetcode/46/NoSameNum.java
@@ -17,15 +17,20 @@ public class NoSameNum {
 
     public static void backtrack(int n, ArrayList<Integer> nums, List<List<Integer>> output, int first) {
         // if all integers are used up
+        //当first走到最后说明nums内已经排列完毕
         if (first == n)
             output.add(new ArrayList<Integer>(nums));
+        //nums中first以前的索引已经排列完毕
+        //现在开始搜索此时first能够指向的值（遍历first以后的索引）
         for (int i = first; i < n; i++) {
             // place i-th integer first
             // in the current permutation
+            //将遍历到的值位置转换到first处
             Collections.swap(nums, first, i);
             // use next integers to complete the permutations
+            //开始探索
             backtrack(n, nums, output, first + 1);
-            // backtrack
+            // backtrack 回溯，将遍历到的值放回原处，开始下一次遍历
             Collections.swap(nums, first, i);
         }
     }

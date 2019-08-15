e251bb5b506dd724aace9ce9f6507522a1ec6731
AutoXC <43327341+AutoXC@users.noreply.github.com>
2019-08-15 14:48:46 +0800

    Update XuanzhuanListnode.java

--- leetcode/61/XuanzhuanListnode.java
+++ leetcode/61/XuanzhuanListnode.java
@@ -1,47 +1,27 @@
 package no61;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class XuanzhuanListnode {
     public static void main(String[] args) {
         
     }
     
-    public ListNode rotateRight(ListNode head, int k) {
-        if(head==null||k==0) return head;
-        LinkedList<ListNode> temp = new LinkedList<ListNode>();
+    public static ListNode rotateRight(ListNode head, int k) {
+        if(head==null||k==0||head.next==null) return head;
+        ArrayList<ListNode> temp = new ArrayList<ListNode>();
         while(head!=null) {
-            temp.addLast(head);
+            temp.add(head);
             head = head.next;
         }
         k %= temp.size();
-        if(k==0) return temp.getFirst();
-        while(k--!=0) {
-            temp.addFirst(temp.removeLast());
-        }
-        for(int i = 0;i<temp.size();i++) {
-            if(i==temp.size()-1) {temp.get(i).next=null;}
-            temp.get(i).next = temp.get(i+1);
-        }
-        return temp.getFirst();
-    }
-    
-    public ListNode rotateRight0(ListNode head, int k) {
-        if(head==null||k==0) return head;
-        int size = 0;
-        ListNode temp = head;
-        ListNode index = null;
-        while(temp!=null) {
-            size++;
-            if(temp.next!=null) {index = temp;}
-            temp = temp.next;
-        }
-        k %= size;
-        if(k==0||size==1) return head;
-        while(k--!=0) {
-            head = index.next;
-            index.next=null;
+        if(k==0) return temp.get(0);
+        else {
+            temp.get(temp.size()-1).next = temp.get(0);
+            temp.get(temp.size()-k-1).next = null;
         }
         
+        return temp.get(temp.size()-k);
     }
+    
 }
